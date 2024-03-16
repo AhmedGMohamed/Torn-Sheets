9dd2a58b621877c303f609f8e91e4c5acbed820a
Ahmed Gamal <ahmedg.mohamed07@gmail.com>
2024-03-16 22:30:38 +0200

    Basic server setup & start of code refactoring

---
+++ utils/clearFormat.js
@@ -0,0 +1,45 @@
+/**
+ * clears the spreadsheet formatting for use
+ * 
+ * @param {sheets_v4.Sheets} sheets a spreadsheets instance
+ */
+async function clearSpreadsheetFormatting(sheets) {
+	try {
+		let backgroundColorStyleDefault = {
+				rgbColor: {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1
+				}
+			},
+			foregroundColorStyleDefault = {
+				rgbColor: {
+					red: 0,
+					green: 0,
+					blue: 0,
+					alpha: 0
+				}
+			},
+			requests = [
+				getRepeatCellRequest(
+					[0, 0, 1000, 0, 1000],
+					null,
+					backgroundColorStyleDefault,
+					foregroundColorStyleDefault,
+					10,
+					false,
+					null,
+					null
+				)
+			];
+		await sheets.spreadsheets.batchUpdate({
+			spreadsheetId: SPREADSHEET_ID,
+			requestBody: {
+				requests: requests
+			}
+		});
+	} catch (error) {
+		console.error("an error occured while clearing the spreadsheet", error);
+	}
+}
---
+++ utils/clearValues.js
@@ -0,0 +1,50 @@
+/**
+ * clears the spreadsheet cells values for use
+ *
+ * @param {sheets_v4.Sheets} sheets a spreadsheets instance
+ */
+async function clearSpreadsheetValues(sheets) {
+	try {
+		await sheets.spreadsheets.values.clear({
+			spreadsheetId: SPREADSHEET_ID,
+			range: "Cache Prices!A1:ZZ1000"
+		});
+		let backgroundColorStyleDefault = {
+				rgbColor: {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1
+				}
+			},
+			foregroundColorStyleDefault = {
+				rgbColor: {
+					red: 0,
+					green: 0,
+					blue: 0,
+					alpha: 0
+				}
+			};
+		await sheets.spreadsheets.batchUpdate({
+			spreadsheetId: SPREADSHEET_ID,
+			requestBody: {
+				requests: [
+					getRepeatCellRequest(
+						[0, 0, 1000, 0, 1000],
+						null,
+						backgroundColorStyleDefault,
+						foregroundColorStyleDefault,
+						10,
+						false,
+						null,
+						null
+					)
+				]
+			}
+		});
+	} catch (error) {
+		console.error("an error occured while clearing the spreadsheet", error);
+	}
+}
+
+export default clearSpreadsheetValues;
