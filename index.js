329a396eff2b8a2aa11a90246686b4135a3ddbbf
Ahmed Gamal <ahmedg.mohamed07@gmail.com>
2024-03-16 18:55:26 +0200

    Updated the google authorization function to use private keys instead of oauth

--- index.js
+++ index.js
@@ -11,70 +11,25 @@ const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), "token.json");
-const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 const ITEMS_PATH = path.join(process.cwd(), "items.json");
 const TORN_API_KEY = process.env.TORN_API_KEY;
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
-
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-	try {
-		const content = await fs.readFile(TOKEN_PATH);
-		const credentials = JSON.parse(content);
-		return google.auth.fromJSON(credentials);
-	} catch (err) {
-		return null;
-	}
-}
-
-/**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-	const content = await fs.readFile(CREDENTIALS_PATH),
-		keys = JSON.parse(content),
-		key = keys.installed || keys.web,
-		payload = JSON.stringify({
-			type: "authorized_user",
-			client_id: key.client_id,
-			client_secret: key.client_secret,
-			refresh_token: client.credentials.refresh_token
-		});
-
-	await fs.writeFile(TOKEN_PATH, payload);
-}
+const CLIENT_EMAIL = process.env.CLIENT_EMAIL;
+const GOOGLE_API_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
 
 /**
  * Load or request or authorization to call APIs.
  *
  */
-async function authorize() {
-	let client = await loadSavedCredentialsIfExist();
-
-	if (client) {
-		return client;
-	}
+function authorize() {
+	let client;
 	try {
-		client = await authenticate({
-			scopes: SCOPES,
-			keyfilePath: CREDENTIALS_PATH
-		});
+		client = new google.auth.JWT(CLIENT_EMAIL, null, GOOGLE_API_KEY, SCOPES);
 	} catch (error) {
 		console.error(
 			"An error occurred while trying to authenticate google api",
 			error
 		);
-	}
-	if (client.credentials) {
-		await saveCredentials(client);
 	}
 	return client;
 }
@@ -690,6 +645,6 @@ async function formatSpreadsheet(auth) {
 	}
 }
 
-const auth = await authorize();
+const auth = authorize();
 const batchData = await fillSpreadsheet(auth);
 await formatSpreadsheet(auth, batchData);
